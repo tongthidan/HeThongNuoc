c0f7ff80f0bc94309c252fa2fd42377616632a45
Hoà Béo <lucifer7997@gmail.com>
2021-04-25 22:22:01 +0700

    add

---
+++ src/main/java/com/example/demo/HeThongNuocApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongNuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeThongNuocApplication.class, args);
+	}
+
+}
