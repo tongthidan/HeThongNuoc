e176bb0c7e7c364982e85dae3ada7bfad561f5e1
Hoà Béo <lucifer7997@gmail.com>
2021-04-28 22:35:52 +0700

    add

--- src/main/java/api/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Bill;
-
-public interface BillRepository extends JpaRepository<Bill, Integer>{
-
-}
--- src/main/java/api/TaxesRepository.java
+++
@@ -1,9 +0,0 @@
-package api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Taxes;
-
-public interface TaxesRepository extends JpaRepository<Taxes, Integer>{
-
-}
