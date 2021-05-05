34e18970c6953adc9663524d26841a4bbe7ef1a8
tongthidan <tongthidan99@gmail.com>
2021-05-05 16:12:02 +0700

    CRUD MoneyUnit, Taxes

--- hethongnuoc-v1/src/main/java/com/example/Repository/TaxesRepository.java
+++ hethongnuoc-v1/src/main/java/com/example/Repository/TaxesRepository.java
@@ -1,9 +1,11 @@
 package com.example.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Entity.Taxes;
 
-public interface TaxesRepository extends JpaRepository<Taxes, String>{
-
+public interface TaxesRepository extends JpaRepository<Taxes, Long>{
+	List<Taxes> findByNameTaxes(String nameTaxes);
 }
